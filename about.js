f757f7d6c70f719d5a2a74cc2ec086616710d3e6
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2021-10-27 18:53:33 +0200

    Finished audio module

--- about.js
+++ about.js
@@ -1,70 +1,38 @@
 
-
-document.body.style.backgroundColor = "#141414";
-document.body.style.width = "70%";
-document.body.style.height= "100%";
-
-let buttons = [];
-let labels = [["HOME", "./index.html"], ["ABOUT", "./about.html"], ["CONTACT", "./contact.html"]];
-
 let about_text = "I am Timothy Schoen, <br><br> I am a developer and music enthousiast, based in The Netherlands. I develop creative audio effects as Octagon Audio.<br><br>I currently work as a DSP developer for Golden Hearing, a company that strives to improve music perception and enjoyment for people with hearing aids.<br><br>I currently study Music and Technology at HKU (University of the Arts, Utrecht). In 2020, I had an internship with plugin developers AudioEase, creators of AltiVerb.<br><br>If you have an interest in any of these subjects, don't hesitate to contact me, I'll always reply!<br><br>";
 
 function about() {
 
-    let divisor = document.body.clientWidth > 1200 ? 6.0 : 12.0;
-    let left =  document.body.clientWidth / divisor;
-    let width =  document.body.clientWidth - (2 * (document.body.clientWidth / divisor));
-
-    let content = document.createElement("DIV");
-    content.style.cssText = "border-radius:0%; border:none; outline:none; background-color:#141414;";
-    content.style.position = "fixed";
-    content.style.left = "x0px".replace("x0", left);
-    content.style.width = "x0px".replace("x0", width);
-    content.style.height = document.body.clientHeight;
-    document.body.appendChild(content);
-
-    let line = document.createElement("DIV");
-    line.style.cssText = "border-radius:0%; border:none; outline:none; font-size:12px; color:white; background-color:#f0f0f0; position:absolute; top:55px; left:0px;";
-    line.style.height = "2px";
-    line.style.width = "x0px".replace("x0", width);
-    content.appendChild(line);
-
-    var logo = document.createElement("img");
-    logo.src = "./resources/Octagon_extra.png";
-
-    logo.style.width = "130px";
-    logo.style.height = "auto";
-    logo.style.top = "0px";
-    logo.style.left = "12px";
-    logo.style.position = "absolute";
-    content.appendChild(logo);
-
-    for (let i = 0; i < 3; i++) {
-      buttons[i] = document.createElement("BUTTON");
-      buttons[i].innerHTML = labels[i][0];
-      buttons[i].style.cssText = "border-radius:0%; border:none; outline:none; font-size:12px; color:white; background-color:transparent; position:absolute;  top:2px; left:x0px;".replace("x0", 170 + (i * 80));
-      buttons[i].className = "button";
-      buttons[i].style.height = "48px";
-      buttons[i].style.width = "80px";
-      buttons[i].style.fontFamily = "LucidaGrande";
-      buttons[i].style.zIndez = "5";
-      buttons[i].onclick = function() {
-        location.href = labels[i][1];
-      }
-      //buttons[i].addEventListener("click", buttonpresets[i][2]);
-      content.appendChild(buttons[i]);
-    }
+    let container = document.createElement("DIV");
+    container.style.padding = "50px";
+    container.style.margin = "50px";
+    container.style.cssText = "font-family: LucidaGrande; border-radius:5px; border:2px solid white; outline:none; font-size:14px; color:white; background-color:transparent; position:fixed; left:0px;";
+    container.style.height = "300px";
+    container.style.left = left + "px";
+    container.style.top = "80px";
+    container.style.width = width + "px";
 
     let about_me = document.createElement("DIV");
     about_me.innerHTML += about_text;
-    about_me.style.cssText = "font-family: LucidaGrande; border-radius:5px; border:2px solid white; outline:none; font-size:14px; color:white; background-color:transparent; position:fixed; left:0px;";
+    about_me.style.cssText = "font-family: LucidaGrande; outline:none; font-size:14px; color:white; background-color:transparent; position:relative; left:0px;";
     about_me.style.height = "300px";
-    about_me.style.left = (left - 50) + "px";
-    about_me.style.top = "60px";
-    about_me.style.width = (width - 100) + "px";
-    about_me.style.padding = "50px";
-    about_me.style.margin = "50px";
-    document.body.appendChild(about_me);
+    about_me.style.left =  "250px";
+    about_me.style.top = "30px";
+    about_me.style.width = "600px"
+    container.appendChild(about_me);
+
+/*
+    let my_face = document.createElement("DIV");
+    my_face.innerHTML += about_text;
+    my_face.style.cssText = "font-family: LucidaGrande; border:0px; outline:none; font-size:14px; color:white; background-color:transparent; position:relative; left:0px;";
+    my_face.style.height = "300px";
+    my_face.style.left = (left - 50) + "px";
+    my_face.style.top = "60px";
+    my_face.style.width = (width - 100) + "px";
+    container.appendChild(my_face); */
+
+
+    document.body.appendChild(container);
 }
 
 about();
--- main.js
+++ main.js
@@ -1,12 +1,4 @@
 
-
-document.body.style.backgroundColor = "#141414";
-document.body.style.width = "70%";
-document.body.style.height= "100%";
-
-let buttons = [];
-let labels = [["HOME", "./index.html"], ["ABOUT", "./about.html"], ["CONTACT", "./contact.html"]];
-
 let trial_desc = "Try the plugin out in your browser, use Chrome for best results. Saving and deleting presets is disabled."
 
 let products = [
@@ -21,60 +13,6 @@ function main() {
 
     document.body.style.height = ((products.length * 300) + 100) + "px";
 
-    let divisor = document.body.clientWidth > 1200 ? 6.0 : 12.0;
-    let left =  document.body.clientWidth / divisor;
-    let width =  document.body.clientWidth - (2 * (document.body.clientWidth / divisor));
-
-    let content = document.createElement("DIV");
-    content.style.cssText = "border-radius:0%; border:none; outline:none; background-color:#141414;";
-    content.style.position = "absolute";
-    content.style.left = "x0px".replace("x0", left);
-    content.style.width = "x0px".replace("x0", width + 30);
-    content.style.height = "2000px";
-    document.body.appendChild(content);
-
-    let topbar = document.createElement("DIV");
-    topbar.style.cssText = "border-radius:0%; border:none; outline:none; font-size:12px; color:white; background-color:#141414; position:fixed; top:0px; left:0px;";
-    topbar.style.height = "55px";
-    topbar.style.left = left + "px";
-    topbar.style.width = "x0px".replace("x0", width + 30);
-    topbar.style.zIndex = "100";
-    content.appendChild(topbar);
-
-    let line = document.createElement("DIV");
-    line.style.cssText = "border-radius:0%; border:none; outline:none; font-size:12px; color:white; background-color:#f0f0f0; position:fixed; top:55px;";
-    line.style.height = "2px";
-    line.style.zIndex = "100";
-    line.style.left = left + "px";
-    line.style.width = "x0px".replace("x0", width + 30);
-    content.appendChild(line);
-
-    var logo = document.createElement("img");
-    logo.src = "./resources/Octagon_extra.png";
-
-    logo.style.width = "130px";
-    logo.style.height = "auto";
-    logo.style.top = "0px";
-    logo.style.left = (left + 12) + "px";
-    logo.style.position = "fixed";
-    logo.style.zIndex = "100";
-    content.appendChild(logo);
-
-    for (let i = 0; i < 3; i++) {
-      buttons[i] = document.createElement("BUTTON");
-      buttons[i].innerHTML = labels[i][0];
-      buttons[i].style.cssText = "border-radius:0%; border:none; outline:none; font-size:12px; color:white; background-color:transparent; position:fixed;  top:2px; left:x0px;".replace("x0", left + 170 + (i * 80));
-      buttons[i].className = "button";
-      buttons[i].style.height = "48px";
-      buttons[i].style.width = "80px";
-      buttons[i].style.fontFamily = "LucidaGrande";
-      buttons[i].style.zIndex = "100";
-      buttons[i].onclick = function() {
-        location.href = labels[i][1];
-      }
-      //buttons[i].addEventListener("click", buttonpresets[i][2]);
-      content.appendChild(buttons[i])
-    }
 
     for (let i = 0; i < products.length; i++) {
 
@@ -87,11 +25,11 @@ function main() {
       console.log(name);
 
       let panel = document.createElement("DIV");
-      panel.style.cssText = "border-radius:5px; border:2px solid product_color; outline:none; font-size:12px; color:white; background-color:#141414; position:relative; top:100px; left:0px;".replace("product_color", color);
+      panel.style.cssText = "border-radius:5px; border:2px solid product_color; outline:none; font-size:12px; color:white; background-color:#141414; position:relative; top:100px; left:-10px;".replace("product_color", color);
       panel.style.padding = "-20px";
       panel.style.margin = "20px";
       panel.style.height = "300px";
-      panel.style.width = "x0px".replace("x0", width);
+      panel.style.width = "100%";
       panel.style.zIndex = "1";
 
       let png = document.createElement("img");
@@ -117,7 +55,7 @@ function main() {
       product_desc.style.cssText = "font-family: LucidaGrande; border-radius:0%; border:none; outline:none; font-size:20px; color:product_color; background-color:transparent; position:absolute; top:130px; left:0px;".replace("product_font", font).replace("product_color", color)
       product_desc.style.height = "70px";
       product_desc.style.left = "40px";
-      product_desc.style.width = "600px";
+      product_desc.style.width = "50%";
       panel.appendChild(product_desc);
 
       let info_button = document.createElement("BUTTON");
@@ -183,53 +121,59 @@ function main() {
 
       try_button.onclick = function() {
 
-       var iframe = document.createElement("iframe");
-       iframe.setAttribute("src", "./playtest.html");
-       iframe.style.position = "absolute";
-       iframe.style.bottom="1%"
-       iframe.style.right="2%"
-       iframe.style.border = "none";
-       iframe.width = "460px";
-       iframe.height = "360px";
+         var iframe = document.createElement("iframe");
+         iframe.setAttribute("src", "./playtest.html");
+         iframe.style.position = "absolute";
+         iframe.style.bottom="1%"
+         iframe.style.right="2%"
+         iframe.style.border = "none";
+         iframe.width = "660px";
+         iframe.height = "360px";
 
-       panel.appendChild(iframe);
+         panel.appendChild(iframe);
 
-       panel.style.height = "370px";
-       panel.removeChild(info_button);
-       panel.removeChild(try_button);
-       panel.removeChild(product_desc);
-       panel.removeChild(png);
+         panel.style.height = "370px";
+         panel.removeChild(info_button);
+         panel.removeChild(try_button);
+         panel.removeChild(product_desc);
+         panel.removeChild(png);
 
-       let trial_description = document.createElement("DIV");
-       trial_description.innerHTML += trial_desc;
-       trial_description.style.cssText = "font-family: LucidaGrande; border-radius:0%; border:none; outline:none; font-size:15px; color:product_color; background-color:transparent; position:absolute; top:130px; left:0px;".replace("product_font", font).replace("product_color", color)
-       trial_description.style.height = "70px";
-       trial_description.style.left = "40px";
-       trial_description.style.width = "400px";
-       panel.appendChild(trial_description);
+         let trial_description = document.createElement("DIV");
+         trial_description.innerHTML += trial_desc;
+         trial_description.style.cssText = "font-family: LucidaGrande; border-radius:0%; border:none; outline:none; font-size:15px; color:product_color; background-color:transparent; position:absolute; top:130px; left:0px;".replace("product_font", font).replace("product_color", color)
+         trial_description.style.height = "70px";
+         trial_description.style.left = "40px";
+         trial_description.style.width = (width - 700) + "px";
+         panel.appendChild(trial_description);
+
+         if((width - 660) < 100) {
+           trial_description.style.display = "none";
+           product_logo.style.display = "none";
+         }
 
 
-       var close = document.createElement("BUTTON");
-       close.innerHTML = "<";
-       close.style.cssText = "font-family: LucidaGrande; border-radius:5px; border:2px solid; font-size:14px; color:product_color; background-color:transparent; position:absolute; top:10px; left:0px;".replace("product_font", font).replace("product_color", color)
-       close.style.borderColor = color;
-       close.style.height = "30px";
-       close.style.left = "10px";
-       close.style.width = "30px";
-       close.onclick = function() {
-         panel.removeChild(close);
-         panel.removeChild(iframe);
-         panel.removeChild(trial_description);
+         var close = document.createElement("BUTTON");
+         close.innerHTML = "<";
+         close.style.cssText = "font-family: LucidaGrande; border-radius:5px; border:2px solid; font-size:14px; color:product_color; background-color:transparent; position:absolute; top:10px; left:0px;".replace("product_font", font).replace("product_color", color)
+         close.style.borderColor = color;
+         close.style.height = "30px";
+         close.style.left = "10px";
+         close.style.width = "30px";
+         close.onclick = function() {
+           panel.removeChild(close);
+           panel.removeChild(iframe);
+           panel.removeChild(trial_description);
 
-         panel.appendChild(info_button);
-         panel.appendChild(try_button);
-         panel.appendChild(product_desc);
-         panel.appendChild(png);
+           panel.appendChild(info_button);
+           panel.appendChild(try_button);
+           panel.appendChild(product_desc);
+           panel.appendChild(png);
 
-         panel.style.height = "300px";
-       }
+           panel.style.height = "300px";
+           product_logo.style.display = "block";
+         }
 
-       panel.appendChild(close);
+         panel.appendChild(close);
       }
       panel.appendChild(try_button);
 
