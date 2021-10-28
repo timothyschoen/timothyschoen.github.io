42bc8d52d0ef14197909f28381e3eee30a6a4b76
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2021-10-28 18:10:35 +0200

    Footer fix

--- footer.js
+++ footer.js
@@ -2,9 +2,11 @@
 
 function add_footer() {
 	let footer = document.createElement("DIV");
+	footer.innerHTML += "Copyright ©2021 Octagon Audio | All rights reserved";
 	footer.cssText += panel_css.replaceAll("$product_color", "white");
 	footer.style.width = "100%";
 	footer.style.height = "50px";
+	content.appendChild(footer);
 }
 
 add_footer();
